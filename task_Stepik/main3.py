d6e09eaef3a709c9ad2635d2dee4ad7865cdd60f
antonkhas <toha1399@mail.ru>
2023-03-12 21:33:40 +0300

    найти возраст и кол-во самых молодых сотрудников

---
+++ task_Stepik/main3.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+
+
+spark = SparkSession.builder \
+    .master('local[*]') \
+    .appName('Spark_3_task') \
+    .getOrCreate()
+
+data = spark.read.csv('bank.csv', sep=';', header=True)
+
+# Группировка данных по возрасту
+data = data.groupBy('age').count()
+# Нахождение 5 наиболее часто встречающихся возрастов
+data = data.orderBy(F.asc('age'))
+print(data.show(5))
+
+
+
