7533cf167704bb8ff304fd66778ef1e0745ed54b
antonkhas <toha1399@mail.ru>
2023-03-13 15:19:57 +0300

    визуализация данных

--- task_Stepik/main5.py
+++ task_Stepik/main5.py
@@ -1,1 +1,26 @@
 from matplotlib import pyplot as plt
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import avg
+
+spark = SparkSession.builder \
+    .master('local[*]') \
+    .appName('Stepik_4_task') \
+    .getOrCreate()
+
+data = spark.read.csv('stocks_price_final.csv', sep=',', header=True)
+
+
+sec_df = data.select(['sector',
+                      'open',
+                      'close',
+                      'adjusted'])\
+                     .groupBy('sector')\
+                     .agg(avg('open'),
+                          avg('close'),
+                          avg('adjusted'))\
+                     .toPandas()
+ind = list(range(12))
+ind.pop(6)
+sec_df.iloc[ind, :].plot(kind='bar', x='sector', y=sec_df.columns.tolist()[1:],
+                         figsize=(12, 6), ylabel='Stock Price', xlabel='Sector')
+plt.show()
